81da0e11a2c1b90398064510b53f12f29f0716ab
Mikke Schirén <mikke.schiren@wunderkraut.com>
2016-02-11 23:31:35 +0100

    already have and backup, dont redo

--- src/Dropcat/Commands/DbImportCommand.php
+++ src/Dropcat/Commands/DbImportCommand.php
@@ -17,16 +17,12 @@ class DbImportCommand extends Command {
     protected function configure() {
       $path_to_db = '/mydb/backup/db.sql';
       $drush_alias = 'default';
-      $backup_path = '/tmp';
-      $identify_backup = "$drush_alias" . '_' . date("Ymd_His") . '.sql';
       $timeout = '3600';
       $this->setName("dropcat:dbimport")
         ->setDescription("Import db")
         ->setDefinition( array (
           new InputOption('path_to_db', 'p', InputOption::VALUE_OPTIONAL, 'Path to database', $path_to_db),
           new InputOption('drush_alias', 'd', InputOption::VALUE_OPTIONAL, 'Drush alias', $drush_alias),
-          new InputOption('backup_path', 'b', InputOption::VALUE_OPTIONAL, 'Backup path', $backup_path),
-          new InputOption('identify_backup', 'i', InputOption::VALUE_OPTIONAL, 'Identify backup', $identify_backup),
           new InputOption('timeout', 'o', InputOption::VALUE_OPTIONAL, 'Timeout', $timeout),
         ))
         ->setHelp('Import db task');
@@ -35,12 +31,9 @@ class DbImportCommand extends Command {
     protected function execute(InputInterface $input, OutputInterface $output) {
         $drush_alias = $input->getOption('drush_alias');
         $path_to_db = $input->getOption('path_to_db');
-        $backup_path = $input->getOption('backup_path');
-        $identify_backup = $input->getOption('identify_backup');
         $timeout = $input->getOption('timeout');
 
-        $process = new Process("drush @$drush_alias sql-dump > $identify_backup &&
-        drush @$drush_alias sql-drop -y &&
+        $process = new Process("drush @$drush_alias sql-drop -y &&
         drush @$drush_alias sql-cli < $path_to_db");
         $process->setTimeout($timeout);
         $process->run();
