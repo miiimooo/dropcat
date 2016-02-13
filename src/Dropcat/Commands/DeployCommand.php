ca87a71c11f33119d74d28b60263c8e05d1aa3cf
Pixelpiloten <pixelpiloten@gmail.com>
2016-02-13 23:33:21 +0100

    ISSUE-1: Refactored commands run on remote server with ssh-class from phpseclib.

--- src/Dropcat/Commands/DeployCommand.php
+++ src/Dropcat/Commands/DeployCommand.php
@@ -2,6 +2,8 @@
 
 namespace Dropcat\Commands;
 
+use phpseclib\Crypt\RSA;
+use phpseclib\Net\SSH2;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,13 +18,14 @@ class DeployCommand extends Command {
 
     protected function configure()
     {
-      $zip = 'foo.zip';
-      $server = 'localhost';
-      $user = 'ubuntu';
+      $zip = 'dropcat.zip';
+      $server = '192.168.10.20';
+      $user = 'vagrant';
       $target_path = 'foo';
       $port = '22';
+      $identity_file = '/srv/www/wk_basebox/.vagrant/machines/default/virtualbox/private_key';
       $alias = 'default';
-      $web_root = '/var/www/webroot/';
+      $web_root = '/home/vagrant/dropcat/';
       $temp_folder = '/tmp';
       $debug = false;
 
@@ -34,6 +37,7 @@ class DeployCommand extends Command {
              new InputOption('user', 'u', InputOption::VALUE_OPTIONAL, 'User', $user),
              new InputOption('target_path', 't', InputOption::VALUE_OPTIONAL, 'Target path', $target_path),
              new InputOption('port', 'p', InputOption::VALUE_OPTIONAL, 'Port', $port),
+             new InputOption('identity_file', 'i', InputOption::VALUE_OPTIONAL, 'IdentityFile', $identity_file),
              new InputOption('alias', 'a', InputOption::VALUE_OPTIONAL, 'Alias', $alias),
              new InputOption('web_root', 'w', InputOption::VALUE_OPTIONAL, 'Web root', $web_root),
              new InputOption('temp_folder', 'tf', InputOption::VALUE_OPTIONAL, 'Temp folder', $temp_folder),
@@ -45,47 +49,51 @@ class DeployCommand extends Command {
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $zip = $input->getOption('zip');
-        $server = $input->getOption('server');
-        $user = $input->getOption('user');
-        $target_path = $input->getOption('target_path');
-        $port = $input->getOption('port');
-        $web_root = $input->getOption('web_root');
-        $alias = $input->getOption('alias');
-        $temp_folder = $input->getOption('temp_folder');
-        $debug = $input->getOption('debug');
+      $zip = $input->getOption('zip');
+      $server = $input->getOption('server');
+      $user = $input->getOption('user');
+      $target_path = $input->getOption('target_path');
+      $port = $input->getOption('port');
+      $identity_file = $input->getOption('identity_file');
+      $identity_file_content = file_get_contents($identity_file);
+      $web_root = $input->getOption('web_root');
+      $alias = $input->getOption('alias');
+      $temp_folder = $input->getOption('temp_folder');
+      $debug = $input->getOption('debug');
 
-        // Debug mode does not delete anything and does not create the symlink to build
-        if ($debug == true) {
-          $process = new Process("ssh -p $port $user@$server << EOF
-          mkdir $temp_folder/$target_path
-          cp $temp_folder/$zip $temp_folder/$target_path
-          cd $temp_folder/$target_path
-          unzip $zip
-          cd ..
-          cp $target_path $web_root
-EOF");
-        }
-        else  {
-          $process = new Process("ssh -p $port $user@$server << EOF
-          mkdir $temp_folder/$target_path
-          mv $temp_folder/$zip $temp_folder/$target_path
-          cd $temp_folder/$target_path
-          unzip $zip
-          rm *.zip
-          cd ..
-          mv $target_path $web_root
-          cd $web_root
-          rm $alias
-          ln -s $target_path $alias
-EOF");
-        }
+      $ssh = new SSH2($server, $port);
+      $auth = new RSA();
+      $auth->loadKey($identity_file_content);
 
-        $process->run();
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-        echo $process->getOutput();
+      if (!$ssh->login($user, $auth)) {
+        exit('Login Failed');
+      }
+
+      // Debug mode does not delete anything and does not create the symlink to build
+      if ($debug == true) {
+        $ssh->exec('mkdir '. $temp_folder .'/'. $target_path);
+        $ssh->exec('cp '. $temp_folder .'/'. $zip .' '- $temp_folder .'/'. $target_path);
+        $ssh->exec('cd '. $temp_folder .'/'. $target_path);
+        $ssh->exec('unzip '. $zip);
+        $ssh->exec('cd ..');
+        $ssh->exec('cp '. $target_path .' '. $web_root);
+      }
+      else {
+        $ssh->exec('mkdir '. $temp_folder .'/'. $target_path);
+        $ssh->exec('mv '. $temp_folder .'/'. $zip .' '. $temp_folder .'/'. $target_path);
+        $ssh->exec('cd '. $temp_folder .'/'. $target_path);
+        $ssh->exec('unzip '. $zip);
+        $ssh->exec('rm *.zip');
+        $ssh->exec('cd ..');
+        $ssh->exec('mv '. $target_path .' '. $web_root);
+        $ssh->exec('cd '. $web_root);
+        $ssh->exec('rm '. $alias);
+        $ssh->exec('ln -s '. $target_path .' '. $alias);
+      }
+
+      $ssh->disconnect();
+
+      $output->writeln('<info>Task: dropcat:deploy finished</info>');
     }
 }
 
