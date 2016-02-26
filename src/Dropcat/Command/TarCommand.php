e44b0cf36a1bafb2a5ec21e2b91b2268d53ae19e
Henrik Pejer <henrik.pejer@wunderkraut.se>
2016-02-26 10:08:45 +0100

    Removed unused imported classes, removed commented out code

--- src/Dropcat/Command/TarCommand.php
+++ src/Dropcat/Command/TarCommand.php
@@ -7,12 +7,7 @@ use Dropcat\Services\Configuration;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Formatter\OutputFormatter;
 
 class TarCommand extends Command
 {
@@ -35,7 +30,7 @@ class TarCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $ignore_files     = $this->getFilesToIgnore();
+        $ignore_files = $this->getFilesToIgnore();
 
 
         $path_to_app      = $input->getOption('folder');
@@ -51,24 +46,16 @@ class TarCommand extends Command
             var_dump($e);
         }
 
-        /*
-         * $process = new Process("tar $ignore_files -cvf $path_to_tar_file -C $path_to_app .");
-        $process->run();
-          // executes after the command finishes
-          if (!$process->isSuccessful()) {
-              throw new ProcessFailedException($process);
-          }
-          echo $process->getOutput();
-        */
         $output->writeln('<info>Task: dropcat:tar finished</info>');
     }
 
     protected function getFilesToIgnore()
     {
-        $filesToIgnore = \explode(' ',$this->configuration->deployIgnoreFilesTarString());
-        foreach($filesToIgnore as &$file) {
-            $file = str_replace("--exclude='",'', $file);
-            $file = substr($file,0,-1);
+        $filesToIgnore = \explode(' ',
+          $this->configuration->deployIgnoreFilesTarString());
+        foreach ($filesToIgnore as &$file) {
+            $file = str_replace("--exclude='", '', $file);
+            $file = substr($file, 0, -1);
         }
         return $filesToIgnore;
     }
