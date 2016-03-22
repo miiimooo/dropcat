b7fe5ef04294905ecd572610ff7a5007f388effc
Mikke Schirén <mikke.schiren@wunderkraut.com>
2016-03-23 00:00:54 +0100

    renaming and replacing using classes

--- src/Dropcat/Command/InitCommand.php
+++ src/Dropcat/Command/InitCommand.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Process\Process;
-
+use SplFileObject;
 
 class InitCommand extends Command
 {
@@ -64,12 +64,10 @@ To override config in dropcat.yml, using options:
         // Executes after the command finishes.
         if (!$process->isSuccessful()) {
         }
-
         echo $process->getOutput();
 
         $output = new ConsoleOutput();
         $output->writeln('<info>Wk Drupal Template cloned to web_init/web</info>');
-
 
         // Rename files and functions
         $fs = new Filesystem();
@@ -77,31 +75,63 @@ To override config in dropcat.yml, using options:
         // Rename profile to project name
         $fs->rename('web_init/web/profiles/wk-standard', 'web_init/web/profiles/' . $my_profile);
 
-
         // Rename files to project-name
-        $fs->rename('web_init/web/profiles/' . $my_profile . '/wk-standard.profile', 'web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.profile');
-        $fs->rename('web_init/web/profiles/' . $my_profile . '/wk-standard.install', 'web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.install');
-        $fs->rename('web_init/web/profiles/' . $my_profile . '/wk-standard.info.yml', 'web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.info.yml');
+        $fs->rename(
+            'web_init/web/profiles/' . $my_profile . '/wk-standard.profile',
+            'web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.profile'
+        );
+        $fs->rename(
+            'web_init/web/profiles/' . $my_profile . '/wk-standard.install',
+            'web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.install'
+        );
+        $fs->rename(
+            'web_init/web/profiles/' . $my_profile . '/wk-standard.info.yml',
+            'web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.info.yml'
+        );
 
-
+        // Replace in profile composer.json
         // Create uuid for profile
         $uuid = UUID::v4();
-
         // Replace what is needed
-        $path_to_profile_install = 'web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.install';
-        $file_contents = file_get_contents($path_to_profile_install);
-        $file_contents = str_replace("wk-standard_install", "$my_profile" . "_install", $file_contents);
-        $file_contents = str_replace("Install, update and uninstall functions for the wk-standard installation profile.", "Install, update and uninstall functions for $my_profile installation profile.", $file_contents);
-        $file_contents = str_replace("\Drupal::configFactory()->getEditable('system.site')->set('uuid', 'a0bb6f1c-dda6-477b-938a-4f0219775c28')->save(TRUE);", "\Drupal::configFactory()->getEditable('system.site')->set('uuid', '" . $uuid . "')->save(TRUE);", $file_contents);
+        $read = new SplFileObject('web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.install', 'r');
+        $content = $read->fread($read->getSize());
+        $content = str_replace("wk-standard_install", "$my_profile" . "_install", $content);
+        $content = str_replace(
+            "Install, update and uninstall functions for the wk-standard installation profile.",
+            "Install, update and uninstall functions for $my_profile installation profile.",
+            $content
+        );
+        $content = str_replace(
+            "web/profiles/wk-standard/",
+            "web/profiles/$my_profile/",
+            $content
+        );
+        $content = str_replace(
+            "('system.site')->set('uuid', 'a0bb6f1c-dda6-477b-938a-4f0219775c28')->save(TRUE);",
+            "('system.site')->set('uuid', '" . $uuid . "')->save(TRUE);",
+            $content
+        );
+        $write = new SplFileObject($read->getPathname(), 'w+');
+        $write->fwrite($content);
 
-        // Write all to file.
-        file_put_contents($path_to_profile_install, $file_contents);
+        // Replace in profile info file
+        $read = new SplFileObject('web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.info.yml', 'r');
+        $content = $read->fread($read->getSize());
+        $content = str_replace("WK-standard", "$my_profile", $content);
+        $write = new SplFileObject($read->getPathname(), 'w+');
+        $write->fwrite($content);
+
+        // Replace in root composer.json
+        $read = new SplFileObject('web_init/composer.json', 'r');
+        $content = $read->fread($read->getSize());
+        $content = str_replace("web/profiles/wk-standard/", "web/profiles/$my_profile/", $content);
+        $write = new SplFileObject($read->getPathname(), 'w+');
+        $write->fwrite($content);
 
         $output = new ConsoleOutput();
         $output->writeln('<info>Renaming of functions and files finished</info>');
 
         $process = new Process("mv web_init/* . && rm -rf web_init");
-
         $process->run();
         // Executes after the command finishes.
         if (!$process->isSuccessful()) {
