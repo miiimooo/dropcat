69ac13047223243dd512f153f9ffdce1e9f2819e
Mikke Schirén <mikke.schiren@wunderkraut.com>
2016-03-22 12:06:08 +0100

    rollbacked

--- src/Dropcat/Command/InitCommand.php
+++ src/Dropcat/Command/InitCommand.php
@@ -4,7 +4,6 @@ namespace Dropcat\Command;
 
 use Dropcat\Services\Configuration;
 use Dropcat\Lib\UUID;
-use Symfony\Component\Filesystem\Exception\IOException;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -14,6 +13,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Process\Process;
+
 
 class InitCommand extends Command
 {
@@ -29,13 +29,15 @@ class InitCommand extends Command
 
     protected function configure()
     {
-        $HelpText = 'The <info>init</info> command clones the wk-standard drupal setup and replaces profile related.
+        $HelpText = 'The <info>deploy</info> connects to remote server and upload tar and unpack it in path.
 <comment>Samples:</comment>
-The command needs the --profile parameter):
-<info>dropcat init --profile=myprofile</info>';
+To run with default options (using config from dropcat.yml in the currrent dir):
+<info>dropcat deployt</info>
+To override config in dropcat.yml, using options:
+<info>dropcat deploy -server 127.0.0.0 -i my_pub.key</info>';
 
         $this->setName("init")
-            ->setDescription("Init D8 site from template repo")
+            ->setDescription("Init D8 site")
             ->setDefinition(
                 array(
                     new InputOption(
@@ -45,13 +47,6 @@ The command needs the --profile parameter):
                         'Profile name',
                         null
                     ),
-                    new InputOption(
-                        'template',
-                        't',
-                        InputOption::VALUE_OPTIONAL,
-                        'Project template',
-                        'git@gitlab.wklive.net:mikke-schiren/wk-drupal-template.git'
-                    ),
 
                 )
             )
@@ -59,154 +54,62 @@ The command needs the --profile parameter):
     }
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $myProfile = $input->getOption('profile');
-        $myTemplate = $input->getOption('template');
-        if (!isset($myProfile)) {
-            throw new \Exception("You need to specify profile");
-        }
-        if (!isset($myTemplate)) {
-            throw new \Exception("Template does not except empty value");
-        }
 
-        $process = new Process("git clone $myTemplate web_init");
+        $my_profile = $input->getOption('profile');
+
+        // (startdir is needed for application)
+        $process = new Process("git clone git@gitlab.wklive.net:mikke-schiren/wk-drupal-template.git web_init");
 
         $process->run();
         // Executes after the command finishes.
         if (!$process->isSuccessful()) {
-            throw new \Exception("Something went wrong, could not clone $myTemplate");
         }
+
         echo $process->getOutput();
 
         $output = new ConsoleOutput();
         $output->writeln('<info>Wk Drupal Template cloned to web_init/web</info>');
 
+
         // Rename files and functions
-        $fs1 = new Filesystem();
+        $fs = new Filesystem();
 
-        try {
-            // Rename profile to project name
-            $fs1->rename(
-                'web_init/web/profiles/wk-standard',
-                'web_init/web/profiles/' . $myProfile
-            );
+        // Rename profile to project name
+        $fs->rename('web_init/web/profiles/wk-standard', 'web_init/web/profiles/' . $my_profile);
 
-            // Rename files to project-name
-            $fs1->rename(
-                'web_init/web/profiles/' . $myProfile . '/wk-standard.profile',
-                'web_init/web/profiles/' . $myProfile . '/' . $myProfile . '.profile'
-            );
-            $fs1->rename(
-                'web_init/web/profiles/' . $myProfile . '/wk-standard.install',
-                'web_init/web/profiles/' . $myProfile . '/' . $myProfile . '.install'
-            );
-            $fs1->rename(
-                'web_init/web/profiles/' . $myProfile . '/wk-standard.info.yml',
-                'web_init/web/profiles/' . $myProfile . '/' . $myProfile . '.info.yml'
-            );
 
-        } catch (IOExceptionInterface $e) {
-            echo "An error occurred while renaming ".$e->getPath();
-        }
+        // Rename files to project-name
+        $fs->rename('web_init/web/profiles/' . $my_profile . '/wk-standard.profile', 'web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.profile');
+        $fs->rename('web_init/web/profiles/' . $my_profile . '/wk-standard.install', 'web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.install');
+        $fs->rename('web_init/web/profiles/' . $my_profile . '/wk-standard.info.yml', 'web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.info.yml');
 
-        $output = new ConsoleOutput();
-        $output->writeln('<info>Renamed files</info>');
 
         // Create uuid for profile
         $uuid = UUID::v4();
 
-        // Replace what is needed in install.
-        $pathToProfileInstall = 'web_init/web/profiles/' . $myProfile . '/' . $myProfile . '.install';
+        // Replace what is needed
+        $path_to_profile_install = 'web_init/web/profiles/' . $my_profile . '/' . $my_profile . '.install';
+        $file_contents = file_get_contents($path_to_profile_install);
+        $file_contents = str_replace("wk-standard_install", "$my_profile" . "_install", $file_contents);
+        $file_contents = str_replace("Install, update and uninstall functions for the wk-standard installation profile.", "Install, update and uninstall functions for $my_profile installation profile.", $file_contents);
+        $file_contents = str_replace("\Drupal::configFactory()->getEditable('system.site')->set('uuid', 'a0bb6f1c-dda6-477b-938a-4f0219775c28')->save(TRUE);", "\Drupal::configFactory()->getEditable('system.site')->set('uuid', '" . $uuid . "')->save(TRUE);", $file_contents);
 
-        $fileContents = file_get_contents($pathToProfileInstall);
-        $fileContents = str_replace(
-            "wk-standard_install",
-            "$myProfile" . "_install",
-            $fileContents
-        );
-        $fileContents = str_replace(
-            "Install, update and uninstall functions for the wk-standard installation profile.",
-            "Install, update and uninstall functions for $myProfile installation profile.",
-            $fileContents
-        );
-        $fileContents = str_replace(
-            "getEditable('system.site')->set('uuid', 'a0bb6f1c-dda6-477b-938a-4f0219775c28')->save(TRUE);",
-            "getEditable('system.site')->set('uuid', '" . $uuid . "')->save(TRUE);",
-            $fileContents
-        );
-
-        $fs2 = new Filesystem();
-        try {
-            $fs2->remove($pathToProfileInstall);
-            $fs2->dumpFile($pathToProfileInstall, $fileContents);
-        } catch (IOExceptionInterface $e) {
-            echo "An error occurred while replacing content in ".$e->getPath();
-        }
+        // Write all to file.
+        file_put_contents($path_to_profile_install, $file_contents);
 
         $output = new ConsoleOutput();
-        $output->writeln('<info>Profile install file rewritten</info>');
-
-        $pathToComposerJson = 'web_init/composer.json';
-        $fileContents = file_get_contents($pathToComposerJson);
-        $fileContents = str_replace(
-            "web/profiles/wk-standard/modules/contrib/",
-            "web/profiles/" . $myProfile . "/modules/contrib/",
-            $fileContents
-        );
-        $fileContents = str_replace(
-            "web/profiles/wk-standard/themes/contrib/",
-            "web/profiles/" . $myProfile . "/themes/contrib/",
-            $fileContents
-        );
-        $fileContents = str_replace(
-            "web/profiles/wk-standard/composer.json",
-            "web/profiles/". $myProfile . "/composer.json",
-            $fileContents
-        );
-
-        $fs3 = new Filesystem();
-        try {
-            $fs3->remove($pathToComposerJson);
-            $fs3->dumpFile($pathToComposerJson, $fileContents);
-        } catch (IOExceptionInterface $e) {
-            echo "An error occurred while replacing content in ".$e->getPath();
-        }
-
-        $output = new ConsoleOutput();
-        $output->writeln('<info>Path to profile composer.json rewritten</info>');
-
-        $pathToProfileComposerJson = 'web_init/web/profiles/' . $myProfile . '/composer.json';
-        $fileContents = file_get_contents($pathToProfileComposerJson);
-        $fileContents = str_replace(
-            "nodeone/wk-standard",
-            "nodeone/" . $myProfile,
-            $fileContents
-        );
-        $fileContents = str_replace(
-            "Installation profile for wk-standard",
-            "Installation profile for " . $myProfile,
-            $fileContents
-        );
-
-        $fs4 = new Filesystem();
-        try {
-            $fs4->remove($pathToProfileComposerJson);
-            $fs4->dumpFile($pathToProfileComposerJson, $fileContents);
-        } catch (IOExceptionInterface $e) {
-            echo "An error occurred while replacing content in ".$e->getPath();
-        }
-
-        $output = new ConsoleOutput();
-        $output->writeln('<info>Profile composer.json rewritten</info>');
+        $output->writeln('<info>Renaming of functions and files finished</info>');
 
         $process = new Process("mv web_init/* . && rm -rf web_init");
+
         $process->run();
         // Executes after the command finishes.
         if (!$process->isSuccessful()) {
-            throw new \Exception('Something went wrong, folder could not be moved or deleted');
         }
+
         echo $process->getOutput();
 
         $output = new ConsoleOutput();
-        $output->writeln('<info>Site is setup.</info>');
+        $output->writeln('<info>Site is setup in current folder.</info>');
     }
 }
