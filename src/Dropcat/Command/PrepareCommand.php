b829d8a6bba0c70928478996aefacd43f6c3a8da
Mikke Schirén <mikke.schiren@wunderkraut.com>
2016-03-31 23:50:07 +0200

    testing problem with env

--- src/Dropcat/Command/PrepareCommand.php
+++ src/Dropcat/Command/PrepareCommand.php
@@ -15,6 +15,7 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
+
 class PrepareCommand extends Command
 {
     /** @var Configuration configuration */
@@ -139,6 +140,9 @@ To override config in dropcat.yml, using options:
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $input = new ArgvInput();
+        $env = $input->getParameterOption(array('--env', '-e'), getenv('SYMFONY_ENV') ?: 'dev');
+        
         $drush_folder = $input->getOption('drush_folder');
         $drush_alias = $input->getOption('drush_alias');
         $server = $input->getOption('server');
