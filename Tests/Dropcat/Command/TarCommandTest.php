99e6b79fa5acda050fa42a0d3fe60aa39f710662
Henrik Pejer <henrik.pejer@wunderkraut.se>
2016-04-01 09:44:20 +0200

    Updated tar tests

--- Tests/Dropcat/Command/TarCommandTest.php
+++ Tests/Dropcat/Command/TarCommandTest.php
@@ -14,51 +14,74 @@ use Symfony\Component\Console\Tester\CommandTester;
 class TarCommandTest extends \PHPUnit_Framework_TestCase
 {
 
-    function setUp() {
-        $this->conf = $configuration = new Configuration();
+    public function setUp()
+    {
+        $this->conf = $this->getMockBuilder('Dropcat\Services\Configuration')
+            ->getMock();
+
+        $this->conf->method('localEnvironmentTmpPath')->willReturn(realpath(__DIR__));
+        $this->conf->method('localEnvironmentAppName')->willReturn('Tarcommandapptest');
+        $this->conf->method('localEnvironmentAppPath')->willReturn(realpath(__DIR__));
+
+
         $application = new Application();
         $application->add(new TarCommand($this->conf));
-        $command = $application->find('tar');
-        $this->tester =  new CommandTester($command);
+        $command      = $application->find('tar');
+        $this->tester = new CommandTester($command);
     }
 
-    function testTarAppFolder()
+    public function testTarAppFolder()
     {
-        $this->tester->execute(
-          array(
-            'command' => 'tar',
-            '-f'      => realpath(__DIR__)
-          )
-        );
-        $this->assertEquals($this->tester->getDisplay(),
-          'Task: tar finished' . "\n");
-        $this->conf = new Configuration();
 
         $filename = $this->conf->localEnvironmentTmpPath() .
             $this->conf->localEnvironmentAppName() .
             $this->conf->localEnvironmentSeperator() .
             $this->conf->localEnvironmentBuildId() . '.tar';
-            $this->assertFileExists($filename);
+        $this->tester->execute(
+            array(
+                'command' => 'tar',
+            )
+        );
+
+        $this->assertEquals(
+            $this->tester->getDisplay(),
+            'Task: tar finished' . "\n"
+        );
+        $this->assertFileExists($filename);
 
         $tar_library = new Archive_Tar($filename);
 
-        $contents    = $tar_library->listContent();
-        $this->assertEquals(\count($contents), 1);
-        $this->assertEquals($contents[0]['filename'], 'TarCommandTest.php');
+        $contents = $tar_library->listContent();
+        // go through all files in this folder and count them
+        $dh = opendir($this->conf->localEnvironmentAppPath());
+        $counter = 0;
+        $files = array();
+        while (($file = readdir($dh)) !== false) {
+            if ($file[0] !== '.') {
+                $counter++;
+                $files[] = $file;
+            }
+        }
+        $this->assertEquals(\count($contents), $counter);
+
+        foreach($files as $key => $file) {
+            $this->assertEquals($contents[$key]['filename'], $file);
+        }
+
     }
 
     /**
      * @expectedException \RuntimeException
-     * @expectedExceptionMessage Unable to tar folder, Error message: Invalid file list
+     *
      */
-    function testTarError()
+    public function testTarError()
     {
         $t = new StdClass();
         $this->tester->execute(
-          array(
-            'command' => 'tar',
-            '-f'      => $t
-          )
+            array(
+                'command' => 'tar',
+                '-f'      => $t
+            )
         );
     }
 }
