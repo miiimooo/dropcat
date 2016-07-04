4e665a1577c5895298d2f4ef06699def129e62dd
Mikke Schirén <mikke.schiren@wunderkraut.com>
2016-07-04 12:10:30 +0200

    missing dependencies

--- src/Dropcat/Command/RunCommand.php
+++ src/Dropcat/Command/RunCommand.php
@@ -4,6 +4,11 @@ namespace Dropcat\Command;
 
 use Dropcat\Services\Configuration;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class RunCommand extends Command
 {
