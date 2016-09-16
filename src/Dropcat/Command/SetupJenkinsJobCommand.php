933ee82626aacae15845ff614f403a4cfd1bc05c
Henrik Pejer <henrik.pejer@wunderkraut.se>
2016-09-16 16:19:27 +0200

    Update after merge to handle new commands

--- src/Dropcat/Command/SetupJenkinsJobCommand.php
+++ src/Dropcat/Command/SetupJenkinsJobCommand.php
@@ -2,6 +2,7 @@
 
 namespace Dropcat\Command;
 
+use Dropcat\Lib\DropcatCommand;
 use Dropcat\Services\Configuration;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Output\ConsoleOutput;
@@ -11,17 +12,8 @@ use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
-class SetupJenkinsJobCommand extends Command
+class SetupJenkinsJobCommand extends DropcatCommand
 {
-  /** @var Configuration configuration */
-    private $configuration;
-
-    public function __construct(Configuration $conf)
-    {
-        $this->configuration = $conf;
-        parent::__construct();
-    }
-
     protected function configure()
     {
         $HelpText = '<info>Setup Jenkins job on server.</info>';
