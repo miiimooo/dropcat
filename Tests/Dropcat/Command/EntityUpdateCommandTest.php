82e22a76513a5ac8a9d142e472c6856605b72f9c
Henrik Pejer <henrik.pejer@wunderkraut.se>
2016-10-28 10:52:30 +0200

    Adding tests for EntityUpdateCommand

---
+++ Tests/Dropcat/Command/EntityUpdateCommandTest.php
@@ -0,0 +1,102 @@
+<?php
+namespace Dropcat\Tests;
+
+use Dropcat\Services\Configuration;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+
+class EntityUpdateCommandTest extends \PHPUnit_Framework_TestCase
+{
+
+    private $conf;
+    private $application;
+    private $commandMock;
+
+    private $container;
+    private $mock;
+
+    public function setUp()
+    {
+        // building the container!
+        $this->container = new ContainerBuilder();
+
+        // Setting DropcatContainer to the DI-container we use.
+        // This way, it will be available to the command.
+        $this->container->set('DropcatContainer', $this->container);
+
+        $this->conf = $configuration = new Configuration();
+
+        $this->application = new Application();
+
+        // We mock the command so that we later on can test Process.
+        $this->mock        = $this->getMockBuilder('\Dropcat\Command\EntityUpdateCommand')
+            ->setConstructorArgs(array($this->container, $this->conf));
+    }
+
+    public function testEntityUpdate()
+    {
+        $process_mock = $this->createMock('Symfony\Component\Process\Process');
+
+        $process_mock->method('isSuccessful')
+            ->willReturn(true);
+
+        $command_mock = $this->mock->setMethods(['runProcess'])
+            ->getMock();
+
+        $command_mock->expects($this->once())
+            ->method('runProcess')
+            ->with($this->equalTo("drush @mysite entup -y"))
+            ->willReturn($process_mock);
+
+        // Add our mocked command from above.
+        $this->application->add($command_mock);
+
+        // Initiate the tester.
+        $this->tester = new CommandTester($command_mock);
+
+        // Execute the test, with our mocked stuff.
+        $this->tester->execute(
+            array(
+                'command' => 'entity-updat',
+                '-d'      => 'mysite',
+            )
+        );
+    }
+
+    public function testEntityUpdateFailure()
+    {
+        $process_mock = $this->createMock('Symfony\Component\Process\Process');
+
+        $process_mock->method('isSuccessful')
+            ->willReturn(false);
+
+        $this->expectException('\Symfony\Component\Process\Exception\ProcessFailedException');
+        $command_mock = $this->mock->setMethods(['runProcess'])
+            ->getMock();
+
+        $command_mock->expects($this->once())
+            ->method('runProcess')
+            ->with($this->equalTo("drush @mysite entup -y"))
+            ->willReturn($process_mock);
+
+        // Add our mocked command from above.
+        $this->application->add($command_mock);
+
+        // Initiate the tester.
+        $this->tester = new CommandTester($command_mock);
+
+        // Execute the test, with our mocked stuff.
+        $this->tester->execute(
+            array(
+                'command' => 'entity-updat',
+                '-d'      => 'mysite',
+            ),
+            array(
+                'verbosity' => OutputInterface::VERBOSITY_VERBOSE
+            )
+        );
+    }
+}
