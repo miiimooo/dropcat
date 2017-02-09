15f22875c4f4c7ea2932e8b06b1c63640980c79e
Henrik Pejer <henrik.pejer@wunderkraut.se>
2017-02-09 16:42:59 +0100

    Only init filesystem when we need it, ie. when we have config to write

--- src/Dropcat/Command/SettingsFileGeneratorCommand.php
+++ src/Dropcat/Command/SettingsFileGeneratorCommand.php
@@ -15,13 +15,17 @@ use Symfony\Component\Filesystem\Filesystem;
 
 class SettingsFileGeneratorCommand extends DropcatCommand
 {
+    /** @var  Filesystem */
+    private $filesystem;
 
-    public function __construct(ContainerBuilder $container, Configuration $conf) {
-      parent::__construct($container, $conf);
-      $this->filesystem = new Filesystem();
+    public function __construct(
+        ContainerBuilder $container,
+        Configuration $conf
+    ) {
+        parent::__construct($container, $conf);
     }
 
-  protected function configure()
+    protected function configure()
     {
         $HelpText = '<info>Overrides settings.php settings.</info>';
 
@@ -29,54 +33,56 @@ class SettingsFileGeneratorCommand extends DropcatCommand
             ->setDescription("Overrides settings.php settings.")
             ->setHelp($HelpText);
     }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output = new ConsoleOutput();
+        $output            = new ConsoleOutput();
         $new_settings_file = $this->addCustomSettings();
         $output->writeln($new_settings_file);
     }
 
-  /**
-   * 1. Check if we have settings in yml
-   * 2. check if we have settings in command line value
-   * 3. parse the yml
-   * 4. append to settings file ( should be in configs )
-   */
-  protected function addCustomSettings()
-  {
-    $custom_settings = $this->configuration->getCustomSettings();
-    if($custom_settings) {
-      // Make local settings file writable.
-      $this->setLocalSettingsfilePermissions(0777);
-      $local_settingsfile = $this->localSettingsFile();
-      $this->filesystem->dumpFile($local_settingsfile, 'fooofaaaa');
-      // Reset local settings file permissions to normal, not writable.
-      $this->setLocalSettingsfilePermissions(0644);
+    /**
+     * 1. Check if we have settings in yml
+     * 2. check if we have settings in command line value
+     * 3. parse the yml
+     * 4. append to settings file ( should be in configs )
+     */
+    protected function addCustomSettings()
+    {
+        $custom_settings = $this->configuration->getCustomSettings();
+        if ($custom_settings) {
+            $this->filesystem = new Filesystem();
+            // Make local settings file writable.
+            $this->setLocalSettingsfilePermissions(0777);
+            $local_settingsfile = $this->localSettingsFile();
+            $this->filesystem->dumpFile($local_settingsfile, 'fooofaaaa');
+            // Reset local settings file permissions to normal, not writable.
+            $this->setLocalSettingsfilePermissions(0644);
+        }
+        return 'wrote something?';
     }
-    return 'wrote something?';
-  }
 
-  protected function getLocalSettingsfileContent()
-  {
-    $this->filesystem->
-  }
+    protected function getLocalSettingsfileContent()
+    {
+        // $this->filesystem->
+    }
 
-  protected function writeToLocalSettingsfile()
-  {
+    protected function writeToLocalSettingsfile()
+    {
 
-  }
+    }
 
-  protected function setLocalSettingsfilePermissions($permissions = 0644)
-  {
-    $localSettingsFile = $this->localSettingsFile();
-    $this->filesystem->chmod($localSettingsFile, $permissions);
-  }
+    protected function setLocalSettingsfilePermissions($permissions = 0644)
+    {
+        $localSettingsFile = $this->localSettingsFile();
+        $this->filesystem->chmod($localSettingsFile, $permissions);
+    }
 
-  protected function localSettingsFile()
-  {
-    $run_path = getcwd();
-    $app_path = $this->configuration->localEnvironmentAppPath();
-    $settings_filename = $run_path .'/web'. $app_path .'/sites/default/settings.local.php';
-    return $settings_filename;
-  }
+    protected function localSettingsFile()
+    {
+        $run_path          = getcwd();
+        $app_path          = $this->configuration->localEnvironmentAppPath();
+        $settings_filename = $run_path . '/web' . $app_path . '/sites/default/settings.local.php';
+        return $settings_filename;
+    }
 }
