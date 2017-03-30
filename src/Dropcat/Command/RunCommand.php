beb36a723ece8cb1aafa27fbdc8f54ca81e11b0b
Henrik Pejer <henrik.pejer@wunderkraut.se>
2017-03-30 16:55:20 +0200

    The file is actually _needed_ you silly bunt

---
+++ src/Dropcat/Command/RunCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dropcat\Command;
+
+use Dropcat\Lib\DropcatCommand;
+use Dropcat\Services\Configuration;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+class RunCommand extends DropcatCommand
+{
+
+    protected function configure()
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+}
