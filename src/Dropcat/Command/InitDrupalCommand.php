cd149819da58d2a51d5a80b2fe2dbc1c7abdbcdd
Fredric Bergström <fredric@bobodrone.com>
2018-01-05 19:07:05 +0100

    Also first remove the .git folder

--- src/Dropcat/Command/InitDrupalCommand.php
+++ src/Dropcat/Command/InitDrupalCommand.php
@@ -130,7 +130,7 @@ class InitDrupalCommand extends DropcatCommand
 
         $io->note('Renaming of functions and files finished');
         // Move all contents inside web_init to current dir (including files ^.*)
-        $process = new Process("find web_init/ -mindepth 1 -maxdepth 1 -exec mv -t ./ -- {} + && rm -rf web_init");
+        $process = new Process("rm -rf web_init/.git && find web_init/ -mindepth 1 -maxdepth 1 -exec mv -t ./ -- {} + && rm -rf web_init");
         $process->run();
         // Executes after the command finishes.
         if (!$process->isSuccessful()) {
